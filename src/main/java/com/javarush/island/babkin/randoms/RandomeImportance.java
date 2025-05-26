41f02a083e1807a1e0cc20f1341c6a123eb49510
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-05-26 10:42:23 +0300

    Add class RandomeImportance

---
+++ src/main/java/com/javarush/island/babkin/randoms/RandomeImportance.java
@@ -0,0 +1,31 @@
+package com.javarush.island.babkin.randoms;
+
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomeImportance extends Thread {
+    int randomEat;
+    int bound;
+
+    public RandomeImportance(int randomEat){
+        bound = 100 - randomEat;
+    }
+
+    public void setRandomEat() {
+        try {
+            int result = ThreadLocalRandom.current().nextInt(100);
+            System.out.println("Вероятность быть съеденным - " + randomEat + "Thread " + Thread.currentThread().threadId() + " generated " + result);
+
+            Thread.yield();
+
+            if (result > bound) {
+                System.out.println("Съели столько - " + result);
+            } else {
+                System.out.println("Не cъели - " + result);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Exception RandomeImportance");
+        }
+    }
+}
