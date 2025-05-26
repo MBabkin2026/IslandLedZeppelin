6e59cef04eaba9ba558fa7d96bd1c028576554b6
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-05-26 10:33:35 +0300

    Add in class Animal interfaces Eatable, Moveable, Reproducible

--- src/main/java/com/javarush/island/babkin/animals/Animal.java
+++ src/main/java/com/javarush/island/babkin/animals/Animal.java
@@ -1,4 +1,23 @@
 package com.javarush.island.babkin.animals;
 
-abstract public class Animal {
+import com.javarush.island.babkin.interfaces.Eatable;
+import com.javarush.island.babkin.interfaces.Moveable;
+import com.javarush.island.babkin.interfaces.Reproducible;
+
+abstract public class Animal implements Eatable, Moveable, Reproducible {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
 }
