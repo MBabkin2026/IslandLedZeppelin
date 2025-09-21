f69702fbbab014d43171fac317c85eba87a4e55e
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-09-21 12:26:24 +0300

    MOD Class RandomImportance

--- src/main/java/com/javarush/island/babkin/randoms/RandomImportance.java
+++ src/main/java/com/javarush/island/babkin/randoms/RandomImportance.java
@@ -4,8 +4,9 @@ package com.javarush.island.babkin.randoms;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomImportance extends Thread {
-    int randomEat;
+
     int bound;
+    private static final int PERCENT = 100;
 
     public RandomImportance(int randomEat) {
         bound = 100 - randomEat;
@@ -19,23 +20,8 @@ public class RandomImportance extends Thread {
         return ThreadLocalRandom.current().nextDouble(value);
     }
 
-
-
-    public void setRandomEat() {
-        try {
-            int result = ThreadLocalRandom.current().nextInt(100);
-            System.out.println("Вероятность быть съеденным - " + randomEat + "Thread " + Thread.currentThread().threadId() + " generated " + result);
-
-            Thread.yield();
-
-            if (result > bound) {
-                System.out.println("Съели столько - " + result);
-            } else {
-                System.out.println("Не cъели - " + result);
-            }
-        } catch (Exception e) {
-            System.out.println("Exception RandomeImportance");
-        }
+    public static int getRandomEat() {
+        return ThreadLocalRandom.current().nextInt(PERCENT);
     }
     public static int getRandomQuantity(int maxSizeAnimalCell) {
         return ThreadLocalRandom.current().nextInt(maxSizeAnimalCell);
