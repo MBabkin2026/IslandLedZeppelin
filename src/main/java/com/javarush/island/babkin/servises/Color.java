d42f51a8b34d9cda5902fcaffd269fcdbb4d5d4f
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-10-04 11:35:11 +0300

    MOD Class Color

--- src/main/java/com/javarush/island/babkin/servises/Color.java
+++ src/main/java/com/javarush/island/babkin/servises/Color.java
@@ -1,6 +1,6 @@
 package com.javarush.island.babkin.servises;
 
-public class Color {
+public abstract class Color {
     public static final String RESET = "\u001B[0m";
     public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
@@ -18,4 +18,32 @@ public class Color {
     public static final String FILL_PURPLE = "\u001B[45m";
     public static final String FILL_CYAN = "\u001B[46m";
     public static final String FILL_WHITE = "\u001B[47m";
+
+    public static String getPercentAnimal(double val){
+
+        String color = "";
+        if(val <= 10) {
+            color = Color.CYAN;
+        } else if (val <= 20) {
+            color = Color.BLUE;
+        } else if (val <= 30) {
+            color = Color.GREEN;
+        } else if (val <= 40) {
+            color = Color.YELLOW;
+        } else if (val <= 50) {
+            color = Color.RED;
+        } else if (val <= 60) {
+            color = Color.FILL_CYAN;
+        } else if (val <= 70) {
+            color = Color.FILL_BLUE;
+        } else if (val <= 80) {
+            color = Color.FILL_GREEN;
+        } else if (val <= 90) {
+            color = Color.FILL_YELLOW;
+        } else {
+            color = Color.FILL_RED;
+        }
+
+        return color;
+    }
 }
