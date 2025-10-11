f902ed1edf416cb0eb5fbe771f89bb72e899daee
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-10-11 15:40:55 +0300

    MOD Class MapGameLock

--- src/main/java/com/javarush/island/babkin/servises/MapGameLock.java
+++ src/main/java/com/javarush/island/babkin/servises/MapGameLock.java
@@ -6,6 +6,8 @@ import java.util.concurrent.locks.ReentrantLock;
 public class MapGameLock {
     private static final Lock mapGameLock = new ReentrantLock();
 
+    private MapGameLock(){}
+
     public static Lock getMapGameLock() {
         return mapGameLock;
     }
