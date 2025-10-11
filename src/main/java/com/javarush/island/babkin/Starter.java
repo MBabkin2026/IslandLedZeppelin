bba8de2916952c54f2281bc2321447c2d205cfb6
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-10-11 15:07:29 +0300

    MOD rename Class Main Of Starter

---
+++ src/main/java/com/javarush/island/babkin/Starter.java
@@ -0,0 +1,11 @@
+package com.javarush.island.babkin;
+
+import com.javarush.island.babkin.servises.*;
+
+public class Starter {
+    public static void main(String[] args) {
+
+        Runner.run();
+
+    }
+}
