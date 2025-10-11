12d69543f2ee2d44148682a1ac22c883d64a76d9
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-10-11 15:04:37 +0300

    MOD Class ViewAnimal

--- src/main/java/com/javarush/island/babkin/servises/ViewAnimal.java
+++ src/main/java/com/javarush/island/babkin/servises/ViewAnimal.java
@@ -2,15 +2,18 @@ package com.javarush.island.babkin.servises;
 
 import com.javarush.island.babkin.map.MapGame;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.javarush.island.babkin.map.MapGame.*;
 
+
 public class ViewAnimal {
 
+    private static final Lock mapGameLock = MapGameLock.getMapGameLock();
     private static final int LIMIT = 5;
 
     private static final StringBuilder SB_UP = new StringBuilder();
@@ -35,72 +38,109 @@ public class ViewAnimal {
 
 
     public static void doViewAnimal(int viewRow, int viewColumn) {
-        Set<ExampleClass>[][] mapGame = getMapGame();
-        int row = viewRow;
-        int column = viewColumn;
-        int limitAnimalviewInCell = 5;
-        String noAnimalSymbol = "  ";
+        mapGameLock.lock();
+        try {
+            Set<ExampleClass>[][] mapGame = getMapGame();
+            int row = viewRow;
+            int column = viewColumn;
+            int limitAnimalviewInCell = 5;
+            String noAnimalSymbol = "  ";
 
-        for (int i = 0; i < row; i++) {
-            System.out.println(i == 0
-                    ? getUpBorder(row, limitAnimalviewInCell)
-                    : getCenterBorder(row, limitAnimalviewInCell));
-            System.out.print("║");
-
-            for (int j = 0; j < column; j++) {
-                Set<ExampleClass> exampl = mapGame[i][j];
-                StringBuilder cellContent = new StringBuilder();
-
-                long limit = limitAnimalviewInCell;
-                int animalCount = 0;
-
-                for (ExampleClass animal : exampl) {
-                    if (limit-- == 0) break;
-                    if (animal.getMaxSizeAnimalCell() > 0 && animal.getCountAnimal() >= 0) {
-                        double percentageOfMax = (double) animal.getCountAnimal() / animal.getMaxSizeAnimalCell() * 100.0;
-                        cellContent.append(Color.getPercentAnimal(percentageOfMax))
-                                .append(animal.getShortName())
-                                .append(Color.RESET);
-                        animalCount++;
-                    }
-                }
-
-                int symbolsToAdd = limitAnimalviewInCell - animalCount;
-                if (animalCount == 0) {
-                    cellContent.append(noAnimalSymbol.repeat(limitAnimalviewInCell));
-                } else {
-                    cellContent.append(noAnimalSymbol.repeat(Math.max(0, symbolsToAdd)));
-                }
-
-                System.out.print(cellContent);
+            for (int i = 0; i < row; i++) {
+                System.out.println(i == 0
+                        ? getUpBorder(row, limitAnimalviewInCell)
+                        : getCenterBorder(row, limitAnimalviewInCell));
                 System.out.print("║");
+
+                for (int j = 0; j < column; j++) {
+                    Set<ExampleClass> exampl = mapGame[i][j];
+                    StringBuilder cellContent = new StringBuilder();
+
+                    long limit = limitAnimalviewInCell;
+                    int animalCount = 0;
+
+                    for (ExampleClass animal : exampl) {
+                        if (limit-- == 0) break;
+                        if (animal.getMaxSizeAnimalCell() > 0 && animal.getCountAnimal() >= 0) {
+                            double percentageOfMax = (double) animal.getCountAnimal() / animal.getMaxSizeAnimalCell() * 100.0;
+                            cellContent.append(Color.getPercentAnimal(percentageOfMax))
+                                    .append(animal.getShortName())
+                                    .append(Color.RESET);
+                            animalCount++;
+                        }
+                    }
+
+                    int symbolsToAdd = limitAnimalviewInCell - animalCount;
+                    if (animalCount == 0) {
+                        cellContent.append(noAnimalSymbol.repeat(limitAnimalviewInCell));
+                    } else {
+                        cellContent.append(noAnimalSymbol.repeat(Math.max(0, symbolsToAdd)));
+                    }
+
+                    System.out.print(cellContent);
+                    System.out.print("║");
+                }
+                System.out.println();
             }
-            System.out.println();
+            System.out.println(getDownBorder(row, limitAnimalviewInCell));
+            System.out.flush();
+
+            System.out.printf("Scale: %s %s %s %s %s %s %s %s %s %s %n", Color.CYAN + 10, Color.BLUE + 20, Color.GREEN + 30, Color.YELLOW + 40, Color.RED + 50 + Color.RESET,
+                    Color.FILL_CYAN + 60, Color.FILL_BLUE + 70, Color.FILL_GREEN + 80, Color.FILL_YELLOW + 90, Color.FILL_RED + 100 + Color.RESET);
+
+            System.out.println(processAndPrintAnimals(mapGame, mapGameLock, LIMIT));
+
+        } finally {
+            mapGameLock.unlock();
         }
-        System.out.println(getDownBorder(row, limitAnimalviewInCell));
-
-        Set<ExampleClass> animalsInFirstCell = mapGame[0][0];
-        List<ExampleClass> animalList = new ArrayList<>(animalsInFirstCell);
-        animalList.sort(Comparator.comparingInt(ExampleClass::getCountAnimal).reversed());
-
-        animalsInFirstCell.stream()
-                .sorted(Comparator.comparingInt(ExampleClass::getCountAnimal).reversed())
-                .filter(animal -> animal.getMaxSizeAnimalCell() > 0 && animal.getCountAnimal() >= 0)
-                .limit(LIMIT)// Фильтруем элементы
-                .forEach(animal -> {
-                    double percentageOfMax = (double) animal.getCountAnimal() / animal.getMaxSizeAnimalCell() * 100.0;
-                    System.out.printf("%s %s%s: %d %s, ",
-                            animal.getIcons(),
-                            Color.getPercentAnimal(percentageOfMax),
-                            animal.getShortName(),
-                            animal.getCountAnimal(),
-                            Color.RESET
-                    );
-                });
-        System.out.println();
-
 
     }
+
+    public static String processAndPrintAnimals(Set<ExampleClass>[][] mapGame, Lock mapGameLock, int LIMIT){
+        StringBuilder result = new StringBuilder();
+
+        mapGameLock.lock();
+        try {
+            List<ExampleClass> allAnimals = Arrays.stream(mapGame)
+                    .flatMap(Arrays::stream)
+                    .filter(Objects::nonNull)
+                    .flatMap(cell -> {
+                        if (cell instanceof Set) {
+                            return ((Set<ExampleClass>) cell).stream();
+                        } else {
+                            return Stream.of((ExampleClass) cell);
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+
+            allAnimals.sort(Comparator.comparingInt(ExampleClass::getCountAnimal).reversed());
+
+            allAnimals.stream()
+                    .filter(animal -> animal.getMaxSizeAnimalCell() > 0 && animal.getCountAnimal() >= 0)
+                    .limit(LIMIT) //Ограничение по количеству
+                    .forEach(animal -> {
+                        double percentageOfMax = (double) animal.getCountAnimal() / animal.getMaxSizeAnimalCell() * 100.0;
+                        result.append(String.format("%s %s%s: %d %s, ",
+                                animal.getIcons(),
+                                Color.getPercentAnimal(percentageOfMax),
+                                animal.getShortName(),
+                                animal.getCountAnimal(),
+                                Color.RESET
+                        ));
+                    });
+
+            if (result.length() > 0) {
+                result.delete(result.length() - 2, result.length());
+            }
+            return result.toString();
+
+        } finally  {
+            mapGameLock.unlock();
+        }
+    }
+
+
 
     public static StringBuilder getUpBorder(int count, int countSpace) {
         return new StringBuilder().append(SIMBOL_UP_LEFT)
