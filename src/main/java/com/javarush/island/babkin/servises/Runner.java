dead1113a8eb11ae2bca9de9e27a260def9c6c90
Maksim Babkin <mak.babkin.2025@mail.ru>
2025-10-11 15:00:34 +0300

    ADD Class Runner

---
+++ src/main/java/com/javarush/island/babkin/servises/Runner.java
@@ -0,0 +1,54 @@
+package com.javarush.island.babkin.servises;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static com.javarush.island.babkin.servises.ViewAnimal.doViewAnimal;
+
+public class Runner {
+
+    public static void run() {
+
+        GenerationOrganizm.getGenerationOrganizm();
+
+        List<Runnable> task = List.of(
+                () -> MoveAnimal.doMovingAnimals(),
+                () -> EatAnimal.doEatAnimal(),
+                () -> ReproduceAnimal.doReproduceAnimal(),
+                () -> doViewAnimal(5, 5)
+        );
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        boolean variant = true;
+        while (variant) {
+
+            for (Runnable runnable : task) {
+                executor.submit(runnable);
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("The map is empty! We are closing.");
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1000, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (!executor.isTerminated()) {
+            System.err.println("\n" +
+                    "Some tasks were not completed!");
+            executor.shutdownNow();
+        }
+    }
+
+}
